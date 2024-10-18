70199f875cc54a204e511433203b24d76eb88f15
s-chance <entropy.tree@outlook.com>
2024-10-19 00:12:34 +0800

    refactor(common): refactor short chain generation algorithm

--- common/src/main/java/org/entropy/common/util/Base62UrlShortener.java
+++ common/src/main/java/org/entropy/common/util/Base62UrlShortener.java
@@ -1,12 +1,15 @@
 package org.entropy.common.util;
 
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
+
 import java.nio.charset.StandardCharsets;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Base62UrlShortener {
 
     private static final String BASE62_ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     private static final int BASE = BASE62_ALPHABET.length();
+    private static final HashFunction hashFunction = Hashing.murmur3_32_fixed();
 
     /**
      * 将字符串类型的 URL 转换为 Base62 编码的短链。
@@ -15,29 +18,29 @@ public class Base62UrlShortener {
      * @return Base62 编码的短链。
      */
     public static String encode(String url) {
-        // 使用当前时间戳（毫秒）和随机数来增加唯一性
-        long value = 0;
+        return encode(url, 6);
+    }
 
-        for (byte b : url.getBytes(StandardCharsets.UTF_8)) {
-            value = value * 256 + b;
-        }
-        value +=  ThreadLocalRandom.current().nextInt();
+    /**
+     * 将字符串类型的 URL 转换为 Base62 编码的短链。
+     *
+     * @param url       要转换的 URL 字符串。
+     * @param maxLength 最大长度限制。
+     * @return Base62 编码的短链。
+     */
+    public static String encode(String url, int maxLength) {
+        // 使用MurmurHash32算法生成哈希值
+        int hashValues = hashFunction.hashString(url, StandardCharsets.UTF_8).asInt();
+
         StringBuilder encoded = new StringBuilder();
-        while (value > 0) {
-            int remainder = (int) (value % BASE);
+
+        // 使用32位哈希值生成Base62编码
+        while (encoded.length() < maxLength) {
+            // 确保余数为非负数
+            int remainder = (hashValues % BASE + BASE) % BASE;
             encoded.insert(0, BASE62_ALPHABET.charAt(remainder));
-            value /= BASE;
+            hashValues /= BASE;
         }
         return encoded.toString();
     }
-
-//    public static void main(String[] args) {
-//        // 测试编码
-//        String originalUrl = "https://www.example.com/some/long/url/that/needs/to/be/shortened";
-//        String encodedUrl = null;
-//        do {
-//            encodedUrl = encode(originalUrl);
-//            System.out.println("Encoded URL: " + encodedUrl);
-//        } while (encodedUrl.length() != 0);
-//    }
 }
