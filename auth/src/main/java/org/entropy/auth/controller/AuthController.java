d5c71ed6c54c689d5ef5405df94a0b80aae92390
s-chance <entropy.tree@outlook.com>
2024-10-28 10:17:30 +0800

    feat(auth): auth module init

---
+++ auth/src/main/java/org/entropy/auth/controller/AuthController.java
@@ -0,0 +1,14 @@
+package org.entropy.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class AuthController {
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
+}
---
+++ auth/src/test/java/org/entropy/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.entropy.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
