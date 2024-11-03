d4b695a5dbbaf68370d2a5ac65a18d5725c630bf
s-chance <entropy.tree@outlook.com>
2024-11-03 12:08:19 +0800

    feat(feign-api): feign interface loadblance

--- feign-api/src/main/java/org/entropy/feignapi/api/InfoClient.java
+++ feign-api/src/main/java/org/entropy/feignapi/api/InfoClient.java
@@ -3,7 +3,7 @@ package org.entropy.feignapi.api;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "producer", url = "127.0.0.1:7000", path = "/info")
+@FeignClient(name = "producer", path = "/info")
 @Deprecated
 public interface InfoClient {
 
